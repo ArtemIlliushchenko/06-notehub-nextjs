b5322c92bcdbd127495c6fdac3e998671bbd5758
Artem illushchenko <artemillushchenko30@gmail.com>
2026-03-09 13:10:43 +0200

    fix hw 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}
+        suppressHydrationWarning={true}
+      >
         <TanStackProvider>
           <Header />
           <main>{children}</main>
